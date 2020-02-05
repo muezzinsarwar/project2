235c1d3150ae948d182fdfbab3430e61f878b501
muezzinsarwar <muezzinsarwar17@gmail.com>
2020-02-05 13:55:45 -0600

    added a timeout

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ class App extends Component {
 		this.setState({ champion: champ });
 	};
 	render() {
+		setTimeout(() => {}, 2000);
 		return (
 			<div>
 				<nav>
--- src/ChampionInfo.js
+++ src/ChampionInfo.js
@@ -4,13 +4,17 @@ import ChampionItems from './ChampionItems';
 class ChampionInfo extends Component {
 	render() {
 		let champion = this.props.champion;
-		const url = `https://ddragon.leagueoflegends.com/cdn/10.2.1/data/en_US/champion/${champion}.json`;
+		let url = `https://ddragon.leagueoflegends.com/cdn/10.2.1/data/en_US/champion/${champion}.json`;
+		let championName = '';
 		fetch(url)
 			.then(response => response.json())
 			.then(response => {
-				console.log(response);
+				let championInfo = Object.entries(response.data);
+				console.log(response.data);
+				console.log(championInfo);
+				championName = championInfo[0][1].id;
 			});
-		return <div></div>;
+		return <div>Name: {championName}</div>;
 	}
 }
 
