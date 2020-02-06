cef96d7a25ef6864b4786bf90862aa5be010b21a
muezzinsarwar <muezzinsarwar17@gmail.com>
2020-02-06 06:24:39 -0600

    finished css for champinfo page

--- src/ChampionItems.js
+++ src/ChampionItems.js
@@ -22,36 +22,43 @@ class ChampionItems extends Component {
 
 			display = (
 				<div key={championDisplay[0][1].key}>
-					<img src={championImg} alt={championDisplay[0][1].id} />
-					<h3>{championDisplay[0][1].id}</h3>
-					<h3>{championDisplay[0][1].title}</h3>
+					<img
+						className="fullImage"
+						src={championImg}
+						alt={championDisplay[0][1].id}
+					/>
+					<h1 className="cName">{championDisplay[0][1].id}</h1>
+					<h2 className="cTitle">{championDisplay[0][1].title}</h2>
+
 					<h4>Lore</h4>
 					<p>{championDisplay[0][1].lore}</p>
-					<div>
+					<div className="passive">
 						<img src={championPassive} alt={championDisplay[0][1].id} />
-						{championDisplay[0][1].passive.name} -{' '}
+						<h2>Passive: {championDisplay[0][1].passive.name}</h2>
 						{removeTags(championDisplay[0][1].passive.description)}
 					</div>
-					<div>
-						<img src={qSpell} alt={championDisplay[0][1].spells[0].name} />
-						Q: {championDisplay[0][1].spells[0].name} -{' '}
-						{removeTags(championDisplay[0][1].spells[0].description)}
-					</div>
-					<div>
-						<img src={wSpell} alt={championDisplay[0][1].spells[1].name} />
-						W: {championDisplay[0][1].spells[1].name} -{' '}
-						{removeTags(championDisplay[0][1].spells[1].description)}
-					</div>
-					<div>
-						<img src={eSpell} alt={championDisplay[0][1].spells[2].name} />
-						E: {championDisplay[0][1].spells[2].name} -{' '}
-						{removeTags(championDisplay[0][1].spells[2].description)}
-					</div>
-					<div>
-						<img src={rSpell} alt={championDisplay[0][1].spells[3].name} />
-						R: {championDisplay[0][1].spells[3].name} -{' '}
-						{removeTags(championDisplay[0][1].spells[3].description)}
-					</div>
+					<section className="spells">
+						<div className="q spell">
+							<img src={qSpell} alt={championDisplay[0][1].spells[0].name} />
+							<h2>Q: {championDisplay[0][1].spells[0].name}</h2>
+							{removeTags(championDisplay[0][1].spells[0].description)}
+						</div>
+						<div className="w spell">
+							<img src={wSpell} alt={championDisplay[0][1].spells[1].name} />
+							<h2>W: {championDisplay[0][1].spells[1].name}</h2>
+							{removeTags(championDisplay[0][1].spells[1].description)}
+						</div>
+						<div className="e spell">
+							<img src={eSpell} alt={championDisplay[0][1].spells[2].name} />
+							<h2>E: {championDisplay[0][1].spells[2].name}</h2>
+							{removeTags(championDisplay[0][1].spells[2].description)}
+						</div>
+						<div className="r spell">
+							<img src={rSpell} alt={championDisplay[0][1].spells[3].name} />
+							<h2>R: {championDisplay[0][1].spells[3].name}</h2>
+							{removeTags(championDisplay[0][1].spells[3].description)}
+						</div>
+					</section>
 				</div>
 			);
 		}
